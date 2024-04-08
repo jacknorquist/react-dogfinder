59905c687b8969cb2c97719c7ecbce2ec631d002
Jack Norquist <jack.norquist@outlook.com>
2024-04-08 18:47:23 -0500

    Nav link Active

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import './App.css';
 
 
 /**
@@ -12,11 +14,22 @@ import { Link } from "react-router-dom";
  */
 
 function NavBar({ dogs }) {
+    const [activeLink, setActiveLink] = useState(null);
+
+    function onClick(evt) {
+        evt.preventDefault();
+        setActiveLink(evt.target.id);
+    };
     return (
         <div>
-            <Link to="/">Home</Link>
+            <Link id="Home" className={activeLink === 'Home' ? "activeLink" : "link"} to="/" onClick={onClick}>Home</Link>
             <nav className="NavBar">
-                {dogs.map((d, i) => <Link to={`/dogs/${d.name}`} key={i}>{d.name}</Link>)}
+                {dogs.map((d, i) => <Link
+                    className={d.name === activeLink ? "activeLink" : "link"}
+                    to={`/dogs/${d.name}`}
+                    onClick={onClick}
+                    key={d.name}
+                    id={d.name}>{d.name}</Link>)}
             </nav>
         </div>
     );
